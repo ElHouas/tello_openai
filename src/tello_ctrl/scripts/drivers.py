d9402e76e70bbed9581ab6a67cd51649243dcc35
IsmaelElHouas <ismael.ghouddana@wecorpindustries.com>
2020-07-07 09:34:46 +0200

    ADD

--- src/tello_ctrl/scripts/drivers.py
+++ src/tello_ctrl/scripts/drivers.py
@@ -4,10 +4,11 @@
 import threading
 import rospy
 from geometry_msgs.msg import Point, Pose, Quaternion, Twist
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CompressedImage, Imu
 from std_msgs.msg import Empty
 from tello_msgs.msg import FlightData
 from cv_bridge import CvBridge, CvBridgeError
+from nav_msgs.msg import Odometry
 
 #Python Utilities
 import av
@@ -23,6 +24,7 @@ fpv = [960, 720]
 #Classes
 from helpers.control import Control
 control = Control()
+pose = Pose()
 
 class TelloDriver(object):
     def __init__(self):
@@ -35,18 +37,21 @@ class TelloDriver(object):
         # Init 
         rospy.init_node('tello_driver_node', anonymous=False)
         self.current_yaw = 0.0
-        self.rate = rospy.Rate(10)
+        self.rate = rospy.Rate(30)
         self._cv_bridge = CvBridge()
         self.frame = None
         self.centroids = []
+        self.height = 0
 
         # ROS publishers
-        self._flight_data_pub = rospy.Publisher('/tello/flight_data', FlightData, queue_size=10)
-        self._image_pub = rospy.Publisher('/tello/camera/image_raw', Image, queue_size=10)
+        self._flight_data_pub = rospy.Publisher('/tello/flight_data', FlightData, queue_size=1)
+        self._image_pub = rospy.Publisher('/tello/camera/image_raw', Image, queue_size=1)
+        self.pub_odom = rospy.Publisher('/tello/odom', Odometry, queue_size=1, latch=True)
+        self.pub_imu = rospy.Publisher('/tello/imu', Imu, queue_size=1, latch=True)
 
         #  ROS subscribers
         self._drone.subscribe(self._drone.EVENT_FLIGHT_DATA, self.flight_data_callback)
-        #rospy.Subscriber('/tello/odom', Pose, self.pose_callback)
+        self._drone.subscribe(self._drone.EVENT_LOG_DATA, self.cb_data_log)
         rospy.Subscriber("/aiming/target_point", Point, self.point_callback)
 
 
@@ -67,8 +72,6 @@ class TelloDriver(object):
                 frame = deepcopy(self.frame)
                 current_yaw = deepcopy(self.current_yaw)
                 
-                self.centroids = [480,360]
-
                 if len(self.centroids)==0: 
                     continue
                 else:
@@ -77,19 +80,23 @@ class TelloDriver(object):
                     yaw_angle = control.yaw(cent)
 
                     try:
-                        #self._drone.clockwise(yaw_angle)
                         print("yaw_angle", yaw_angle)
+                        self._drone.clockwise(yaw_angle)
+
+                        pose.position = 
+                        pose.orientation = Quaternion(*quaternion_from_euler(0.0, 0.0, yaw_angle*pi/180))
+
 
                     except rospy.ServiceException:
                         pass
 
-                    cv2.circle(frame, (320, cent[1]), 3, [0,0,255], -1, cv2.LINE_AA)
-                    cv2.circle(frame, (cent[0], cent[1]), 3, [0,255,0], -1, cv2.LINE_AA)
+                    cv2.circle(frame, (480, cent[1]), 3, [0,0,255], -1, cv2.LINE_AA) #red
+                    cv2.circle(frame, (cent[0], cent[1]), 3, [0,255,0], -1, cv2.LINE_AA) #green
 
                 cv2.imshow("", frame)
                 cv2.waitKey(1)
                  
-                # print("%s seconds" % (time.time() - start_time))
+                #print("%s seconds" % (time.time() - start_time))
                 #time.sleep((time.time() - start_time)) #slows down twice dont do it
                 
             self.rate.sleep()
@@ -135,6 +142,49 @@ class TelloDriver(object):
         self._drone.land()
         self._drone.quit()
         self._drone = None
+
+    def cb_data_log(self, event, sender, data, **args):
+        time_cb = rospy.Time.now()
+
+        odom_msg = Odometry()
+        odom_msg.child_frame_id = rospy.get_namespace() + 'base_link'
+        odom_msg.header.stamp = time_cb
+        odom_msg.header.frame_id = rospy.get_namespace() + 'local_origin'        
+
+        # Height from MVO received as negative distance to floor
+        odom_msg.pose.pose.position.z = -data.mvo.pos_z #self.height #-data.mvo.pos_z
+        odom_msg.pose.pose.position.x = data.mvo.pos_x
+        odom_msg.pose.pose.position.y = data.mvo.pos_y
+        odom_msg.pose.pose.orientation.w = data.imu.q0
+        odom_msg.pose.pose.orientation.x = data.imu.q1
+        odom_msg.pose.pose.orientation.y = data.imu.q2
+        odom_msg.pose.pose.orientation.z = data.imu.q3
+        # Linear speeds from MVO received in dm/sec
+        odom_msg.twist.twist.linear.x = data.mvo.vel_y/10
+        odom_msg.twist.twist.linear.y = data.mvo.vel_x/10
+        odom_msg.twist.twist.linear.z = -data.mvo.vel_z/10
+        odom_msg.twist.twist.angular.x = data.imu.gyro_x
+        odom_msg.twist.twist.angular.y = data.imu.gyro_y
+        odom_msg.twist.twist.angular.z = data.imu.gyro_z
+                
+        self.pub_odom.publish(odom_msg)
+        
+        imu_msg = Imu()
+        imu_msg.header.stamp = time_cb
+        imu_msg.header.frame_id = rospy.get_namespace() + 'base_link'
+        
+        imu_msg.orientation.w = data.imu.q0
+        imu_msg.orientation.x = data.imu.q1
+        imu_msg.orientation.y = data.imu.q2
+        imu_msg.orientation.z = data.imu.q3        
+        imu_msg.angular_velocity.x = data.imu.gyro_x
+        imu_msg.angular_velocity.y = data.imu.gyro_y
+        imu_msg.angular_velocity.z = data.imu.gyro_z
+        imu_msg.linear_acceleration.x = data.imu.acc_x
+        imu_msg.linear_acceleration.y = data.imu.acc_y
+        imu_msg.linear_acceleration.z = data.imu.acc_z
+        
+        self.pub_imu.publish(imu_msg)
 
 def main():
     try:
