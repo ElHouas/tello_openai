8e9e14dbdb874d2b6d2dce79139d94433fc24857
IsmaelElHouas <ismael.ghouddana@wecorpindustries.com>
2020-07-07 11:47:32 +0200

    MOD: Use PS4 in OOP tello openai and tested with tello drone

--- src/tello_ctrl/scripts/helpers/rc/objects.py
+++ src/tello_ctrl/scripts/helpers/rc/objects.py
@@ -10,127 +10,36 @@ import rospy
 
 
 class JoystickPS4:
-    # d-pad
-    UP = -1  # UP
-    DOWN = -1  # DOWN
-    ROTATE_LEFT = -1  # LEFT
-    ROTATE_RIGHT = -1  # RIGHT
+    def __init__(self):
+        # d-pad
+        self.UP = -1  # UP
+        self.DOWN = -1  # DOWN
+        self.ROTATE_LEFT = -1  # LEFT
+        self.ROTATE_RIGHT = -1  # RIGHT
 
-    # bumper triggers
-    TAKEOFF = 5  # R1
-    LAND = 4  # L1
-    # UNUSED = 7 #R2
-    # UNUSED = 6 #L2
+        # bumper triggers
+        self.TAKEOFF = 5  # R1
+        self.LAND = 4  # L1
+        # UNUSED = 7 #R2
+        # UNUSED = 6 #L2
 
-    # buttons
-    FORWARD = 3  # TRIANGLE
-    BACKWARD = 1  # CROSS
-    LEFT = 0  # SQUARE
-    RIGHT = 2  # CIRCLE
+        # buttons
+        self.FORWARD = 3  # TRIANGLE
+        self.BACKWARD = 1  # CROSS
+        self.LEFT = 0  # SQUARE
+        self.RIGHT = 2  # CIRCLE
 
-    # axis
-    LEFT_X = 0
-    LEFT_Y = 1
-    RIGHT_X = 2
-    RIGHT_Y = 3
-    LEFT_X_REVERSE = 1.0
-    LEFT_Y_REVERSE = -1.0
-    RIGHT_X_REVERSE = 1.0
-    RIGHT_Y_REVERSE = -1.0
-    DEADZONE = 0.08
-
-
-buttons = None
-speed = 100
-throttle = 0.0
-yaw = 0.0
-pitch = 0.0
-roll = 0.0
+        # axis
+        self.LEFT_X = 0
+        self.LEFT_Y = 1
+        self.RIGHT_X = 2
+        self.RIGHT_Y = 3
+        self.LEFT_X_REVERSE = 1.0
+        self.LEFT_Y_REVERSE = -1.0
+        self.RIGHT_X_REVERSE = 1.0
+        self.RIGHT_Y_REVERSE = -1.0
+        self.DEADZONE = 0.08
 
 
-def update(old, new, max_delta=0.3):
-    if abs(old - new) <= max_delta:
-        res = new
-    else:
-        res = 0.0
-    return res
 
-
-def handle_input_event(drone, e):
-    global speed
-    global throttle
-    global yaw
-    global pitch
-    global roll
-    if e.type == pygame.locals.JOYAXISMOTION:
-        # ignore small input values (Deadzone)
-        if -buttons.DEADZONE <= e.value and e.value <= buttons.DEADZONE:
-            e.value = 0.0
-        if e.axis == buttons.LEFT_Y:
-            throttle = update(throttle, e.value * buttons.LEFT_Y_REVERSE)
-            drone.set_throttle(throttle)
-        if e.axis == buttons.LEFT_X:
-            yaw = update(yaw, e.value * buttons.LEFT_X_REVERSE)
-            drone.set_yaw(yaw)
-        if e.axis == buttons.RIGHT_Y:
-            pitch = update(pitch, e.value * buttons.RIGHT_Y_REVERSE)
-            drone.set_pitch(pitch)
-        if e.axis == buttons.RIGHT_X:
-            roll = update(roll, e.value * buttons.RIGHT_X_REVERSE)
-            drone.set_roll(roll)
-    elif e.type == pygame.locals.JOYHATMOTION:
-        if e.value[0] < 0:
-            drone.counter_clockwise(speed)
-        if e.value[0] == 0:
-            drone.clockwise(0)
-        if e.value[0] > 0:
-            drone.clockwise(speed)
-        if e.value[1] < 0:
-            drone.down(speed)
-        if e.value[1] == 0:
-            drone.up(0)
-        if e.value[1] > 0:
-            drone.up(speed)
-    elif e.type == pygame.locals.JOYBUTTONDOWN:
-        if e.button == buttons.LAND:
-            drone.land()
-        elif e.button == buttons.UP:
-            drone.up(speed)
-        elif e.button == buttons.DOWN:
-            drone.down(speed)
-        elif e.button == buttons.ROTATE_RIGHT:
-            drone.clockwise(speed)
-        elif e.button == buttons.ROTATE_LEFT:
-            drone.counter_clockwise(speed)
-        elif e.button == buttons.FORWARD:
-            drone.forward(speed)
-        elif e.button == buttons.BACKWARD:
-            drone.backward(speed)
-        elif e.button == buttons.RIGHT:
-            drone.right(speed)
-        elif e.button == buttons.LEFT:
-            drone.left(speed)
-    elif e.type == pygame.locals.JOYBUTTONUP:
-        if e.button == buttons.TAKEOFF:
-            if throttle != 0.0:
-                print('###')
-                print('### throttle != 0.0 (This may hinder the drone from taking off)')
-                print('###')
-            drone.takeoff()
-        elif e.button == buttons.UP:
-            drone.up(0)
-        elif e.button == buttons.DOWN:
-            drone.down(0)
-        elif e.button == buttons.ROTATE_RIGHT:
-            drone.clockwise(0)
-        elif e.button == buttons.ROTATE_LEFT:
-            drone.counter_clockwise(0)
-        elif e.button == buttons.FORWARD:
-            drone.forward(0)
-        elif e.button == buttons.BACKWARD:
-            drone.backward(0)
-        elif e.button == buttons.RIGHT:
-            drone.right(0)
-        elif e.button == buttons.LEFT:
-            drone.left(0)
 
